de3d78185db3dbc6d3f68dccfdad8db41876ee32
Valentina Valverde <vvgreenville@gmail.com>
2023-09-14 14:46:57 -0400

    Created a drop-down menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,52 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM Content Loaded");
 
-    const filmHere= document.getElementById('filmHere');
+    function get(url) {
+        return fetch(url)
+            .then(function (response) {
+                console.log("RESPONSE:", response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log("DATA:", data);
+                // showFilm(data, filmHere);
+                return data;
+            })
+            .catch(function (err) {
+                console.log("ERROR:", err);
+            })
+    };
 
-    fetch('https://swapi.dev/api/films/')
-        .then(function(response){
-            console.log("RESPONSE:", response);
-            return response.json();
-        })
-        .then(function(data){
-            console.log("DATA:", data);
-            const moo = data.results.map();
-            //I NEED A FUNCTION WITHIN MAP!!!!
-            showFilm(moo, filmHere);
-            return data;
-        })
-        .catch(function(err){
-            console.log("ERROR:", err);
-        })
+    const filmsUrl = 'https://swapi.dev/api/films/';
+
+    get(filmsUrl).then(function (response) {
+        console.log("FILM LIST:", response.results);
+        generateFilmList(response.results.map(x => {
+            console.log(x.title);
+            return x.title;
+        }));
+    });
+
 });
 
-function showFilm(film, element){
-    console.log("FILM:", film);
-    element.innerText = film;
-}
+//this displays the drop-down menu with the titles displayed.
+
+const categoryListForm = document.getElementById('categoryListForm');
+
+function generateFilmList(categoryArray) {
+    console.log(categoryArray);
+    const selectElement = document.createElement('select');
+    categoryArray.map(function (category) {
+        //create option element
+        const option = document.createElement('option');
+        //define option atributes
+        option.value = category;
+        option.text = category;
+        //append option to <select>
+        selectElement.appendChild(option);
+    })
+    //append the <select> to <form>
+    categoryListForm.append(selectElement);
+};
